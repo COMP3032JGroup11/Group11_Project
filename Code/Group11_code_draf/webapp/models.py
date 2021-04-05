e935c02888753a31b3bdcfb808ca1cc3afc5f82a
wuxiulin1999 <1317162075@qq.com>
2021-04-05 18:41:26 +0800

    1

--- Code/Group11_code_draf/webapp/models.py
+++ Code/Group11_code_draf/webapp/models.py
@@ -18,11 +18,28 @@ class User(db.Model):
     city = db.Column(db.String(120), index=True)
     state = db.Column(db.String(120), index=True)
     zip = db.Column(db.String(120), index=True)
-    about = db.Column(db.String(500), index=True, unique=True)
+    about = db.Column(db.String(500), index=True)
+    houses = db.relationship('House', back_populates='user')
     # facebook = db.Column(db.String(120), index=True, unique=True)
     # twitter = db.Column(db.String(120), index=True, unique=True)
     # google = db.Column(db.String(120), index=True, unique=True)
     # linkedin = db.Column(db.String(120), index=True, unique=True)
+
+
+class House(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(300), index=True)
+    size = db.Column(db.Integer, index=True)
+    floor_kind = db.Column(db.Integer, index=True)
+    floor_number = db.Column(db.String(10), index=True)
+    room_number = db.Column(db.Integer, index=True)
+    living_number = db.Column(db.Integer, index=True)
+    bath_number = db.Column(db.Integer, index=True)
+    rent_type = db.Column(db.Boolean, default=False)
+    district_number = db.Column(db.Integer, index=True)
+    community__number = db.Column(db.Integer, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', back_populates='houses')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
